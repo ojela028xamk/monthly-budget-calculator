9d40cf1d9b53414a57138f03bd3e08558967b98c
ojela028xamk <jerelans91@gmail.com>
2025-04-18 11:40:46 +0300

    income component

---
+++ app/components/income.tsx
@@ -0,0 +1,11 @@
+import css from './income.module.scss'
+
+export default function Income() {
+
+  return (
+    <div className={css.income}>
+      <h2>Income</h2>
+    </div>
+  )
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
-import css from './page.module.css'
+import Income from './components/Income'
+import css from './page.module.scss'
 
 export default function Home() {
   return (
     <div className={css.page}>
-      <main className={css.main}>
+      <main>
         <h1>Monthly budget calculator</h1>
+        <Income />
       </main>
-      <footer className={css.footer}></footer>
     </div>
   )
 }
